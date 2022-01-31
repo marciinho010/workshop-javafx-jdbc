aa57c15b51e9053c32857e81ef5ac256eaa29aa6
Paulo Fernandes <marciorocha11@gmail.com>
2022-01-31 20:55:54 -0200

    DepartmentForm (dialog) design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableListBase;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -37,8 +46,9 @@ public class DepartmentListController implements Initializable {
 	
 	
 	@FXML
-	public void onBtNewAction() {
-		
+	public void onBtNewAction(ActionEvent event) { // referencia para o controle que recebeu o evento,condição para acessar o stage pelo currentStage
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 
 	public void setDepartmentService(DepartmentService service) {
@@ -73,11 +83,29 @@ public class DepartmentListController implements Initializable {
 		List<Department> list = service.findAll();
 		obsList =  (ObservableListBase<Department>) FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
-		
-		
-		
-		
-		
 	}
 
+	private void createDialogForm(String absoluteName ,Stage parentStage) {
+		//função para carregar a janela do formulario
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data");
+			dialogStage.setScene(new Scene(pane)); // como é uma nova stage, tem que ter uma nova scene.
+			dialogStage.setResizable(false); // comando se pode ou não ser redimensonada 
+			dialogStage.initOwner(parentStage); // comando que solicita o Stage principal
+			dialogStage.initModality(Modality.WINDOW_MODAL); //metodo que diz se vai ser modal ou outro comportamento, window_modal : tela travada enquanto não fechar, não poderar acessar a janela anterior
+			dialogStage.showAndWait();
+			
+			//quando carregar uma janela de dialogo modal na frente da janela existente, tem que instanciar um novo stage ou seja um palco na frente de outro palco
+			
+		}catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Erro loading view", e.getMessage(), Alert.AlertType.ERROR);
+		}
+		
+		//quando cria janela de dialogo, tem que informar para ela quem quw é o stage que criou a janela de dialogo
+	}
 }
