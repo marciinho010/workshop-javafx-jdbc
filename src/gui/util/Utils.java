48130aeb86d5633325e0d40bcf7514c1ea3acb11
Paulo Fernandes <marciorocha11@gmail.com>
2022-02-09 18:59:19 -0200

    Saving Seller

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -27,6 +27,14 @@ public class Utils {
 			return null;
 		}
 	}
+	
+	public static Double tryParseToDouble(String str) { // para ajudar a converter o valor da caixa para inteiro
+		try {
+			return Double.parseDouble(str);
+		} catch (NumberFormatException e) { // caso estoure uma exceção de numero invalido, irá retornar null
+			return null;
+		}
+	}
 
 	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
 		tableColumn.setCellFactory(column -> {
